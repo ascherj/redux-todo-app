d76c2f526440bac219fa8c3a7808a5ef34992b1a
Jake Ascher <ascherj@gmail.com>
2019-05-20 13:14:25 -0400

    Use React to create and update Counter component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
@@ -11,15 +13,34 @@ const counter = (state = 0, action) => {
   }
 };
 
+const Counter = ({ value, onIncrement, onDecrement }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const store = createStore(counter);
 
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
+    document.getElementById('root')
+  );
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
