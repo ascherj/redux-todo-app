fd6aa965dfaf5d580189956427cc768314232daf
Jake Ascher <ascherj@gmail.com>
2019-06-19 11:39:21 -0400

    Extract Todo and TodoList presentational components

--- src/index.js
+++ src/index.js
@@ -71,6 +71,25 @@ const FilterLink = ({ filter, currentFilter, children }) => {
   );
 };
 
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map((todo) => (
+      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
+  </ul>
+);
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case 'SHOW_ALL':
@@ -106,24 +125,15 @@ class TodoApp extends React.Component {
         >
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map((todo) => (
-            <li
-              key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}
-            >
-              {todo.text}
-            </li>
-          ))}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={(id) =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id: id
+            })
+          }
+        />
         <p>
           Show:{' '}
           <FilterLink filter="SHOW_ALL" currentFilter={visibilityFilter}>
